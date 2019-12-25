b14dffe137d11758d8d8e78d41d5af3d75d13d03
bygagashek <pavelmakarov2107@gmail.com>
2019-12-25 22:54:25 +0300

    create a Authorization

--- article/serializers.py
+++ article/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
+
+from django.contrib.auth.models import User
 from .models import Article
 
 
@@ -7,3 +10,20 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('id', 'title', 'description', 'body', 'author_id')
+
+class UserSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = User
+            fields = ('username', 'email', 'password')
+            extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = User(
+                email=validated_data['email'],
+                username=validated_data['username']
+            )
+            user.set_password(validated_data['password'])
+            user.save()
+            Token.objects.create(user=user)
+            return user
--- article/urls.py
+++ article/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-
-from .views import ArticleViewSet
+from django.urls import path
+from .views import ArticleViewSet, UserCreate, LoginView
 
 router = DefaultRouter()
 router.register(r'articles', ArticleViewSet, basename='user')
 
 urlpatterns = router.urls
+urlpatterns += [
+    path("login/", LoginView.as_view(), name="login"),
+    path("users/", UserCreate.as_view(), name="user_create"),
+]
